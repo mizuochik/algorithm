db56820ddac3131b6c8cef92d82405d463009711
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-05-08 21:13:02 +0900

    Add a test for cursor

--- gapbuffer/gap_buffer_test.go
+++ gapbuffer/gap_buffer_test.go
@@ -10,17 +10,21 @@ import (
 func TestGapBuffer(t *testing.T) {
 	t.Run("Insert()", func(t *testing.T) {
 		tests := []struct {
-			desc string
-			size int
-			s    string
-			want string
+			desc       string
+			size       int
+			s          string
+			wantString string
+			wantCursor int
 		}{
-			{"shorter than size", 10, "ab", "ab"},
+			{"shorter than size", 10, "ab", "ab", 2},
 		}
 		for _, tt := range tests {
 			gb := gapbuffer.New(tt.size)
 			gb.Insert(tt.s)
-			if diff := cmp.Diff(tt.want, gb.String()); diff != "" {
+			if diff := cmp.Diff(tt.wantString, gb.String()); diff != "" {
+				t.Errorf("%s: %s", tt.desc, diff)
+			}
+			if diff := cmp.Diff(tt.wantCursor, gb.Cursor()); diff != "" {
 				t.Errorf("%s: %s", tt.desc, diff)
 			}
 		}
